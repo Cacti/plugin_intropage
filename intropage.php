05057bd70930af774c11ca3afcb992eaf4061bc1
Petr Macek <petr.macek@kostax.cz>
2017-10-26 08:43:00 +0200

    Closes #3

--- intropage.php
+++ intropage.php
@@ -9,7 +9,7 @@ set_default_action();
 
 $lopts = db_fetch_cell('SELECT intropage_opts FROM user_auth WHERE id=' . $_SESSION['sess_user_id']);
 if ($lopts == 1)
-//	top_header();	it shows console menu in separated tab
+//	top_header();	it shows console menu in separated tab - we don't need it
 	general_header();
 
 display_informations();
