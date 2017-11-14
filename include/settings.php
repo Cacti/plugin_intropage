2a9cae63b0266aedffaf827f61a7c425e83436ba
Petr Macek <petr.macek@kostax.cz>
2017-11-14 22:04:27 +0100

    fix default page after login and settings for this

--- include/settings.php
+++ include/settings.php
@@ -8,7 +8,7 @@ function intropage_config_form ()	{
 			"friendly_name" => "Intro Page Options",
 			"method" => "radio",
 			"default" => "0",
-			"description" => "How we should display the intropage.",
+			"description" => "How we should display the intropage. <strong>For users without console access you must choose separated tab</strong>",
 			"value" => "|arg1:intropage_opts|",
 			"items" => array(
 				0 => array(
@@ -45,25 +45,38 @@ function intropage_login_options_navigate ()	{
 	
 	$lopts = db_fetch_cell('SELECT intropage_opts FROM user_auth WHERE id=' . $_SESSION['sess_user_id']);
 	
-	if ($lopts == 1) {
-		header("Location: " . $config['url_path'] . "plugins/intropage/intropage.php");
+	if ($lopts == 1) { // tab
+
+	    // from auth_login.php - graph view or intropage (for users without console)
+	    $lopts = db_fetch_cell('SELECT login_opts FROM user_auth WHERE id=' . $_SESSION['sess_user_id']);
+		if ($lopts == 3)
+		    header('Location: ' . $config['url_path'] . 'graph_view.php' . ($newtheme ? '?newtheme=1':''));
+		else
+	    	    header("Location: " . $config['url_path'] . "plugins/intropage/intropage.php");
 	}
+
+	
 }
 
 function intropage_console_after() {
 	global $config;
 	$lopts = db_fetch_cell('SELECT intropage_opts FROM user_auth WHERE id=' . $_SESSION['sess_user_id']);
+
 	
-	if ($lopts == 1) {
-	} else {
+	if ($lopts == 1) { // in tab
+	} else {  // in console
 		include_once($config['base_path'] . "/plugins/intropage/display.php");
 		display_informations();
+
 
 		//$x['data'] = "You can choose view between separated tab and console, you can set it up in Console -> User Management -> User -> Intropage Options ";
 //		intropage_display_panel(100,"green","Hint",$x);
 //	          intropage_display_panel($size,$value['alarm'],$value['name'],$value);
 	    print "<br/>HINT: You can choose view between separated tab and console, you can set it up in Console -> User Management -> User -> Intropage Options";
 	}
+
+	    intropage_display_hint();
+
 }
 
 function intropage_user_admin_setup_sql_save($save) {
@@ -73,5 +86,44 @@ function intropage_user_admin_setup_sql_save($save) {
 
 	return $save;
 }
+
+///* it works bad with user group
+function intropage_display_hint ()       {
+    global $config;
+
+    $lopts = db_fetch_cell('SELECT login_opts FROM user_auth WHERE id=' . $_SESSION['sess_user_id']);
+    // login options can change user group! 
+
+    switch ($lopts)     { // after login: 3=graphs, 4=intropage tab, 5=intropage in console
+        case "1" :
+        case "2" :
+        echo "select realm_id from user_auth_realm where user_id='" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id=8";
+            if (db_fetch_assoc("select realm_id from user_auth_realm where user_id='" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id=8"))        {
+                echo "<b>Hint: </b>If you want to see Intropage plugin as default page (in console or separated tab), you can set it up in Console -> User Management -> User -> Login Options <br/>";
+            }
+            else	{ // no console right
+                echo "<b>Hint: </b>If you want to see Intropage plugin as default page, <a href=\"" . $config['url_path'] . "plugins/intropage/intropage.php?default=true&how=4\">click here </a><BR/>\n";
+            }
+        break;
+
+        case "3":
+            if (!db_fetch_assoc("select realm_id from user_auth_realm where user_id='" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id=8"))
+                echo "<b>Hint: </b>If you want to see Intropage as default page <a href=\"" . $config['url_path'] . "plugins/intropage/intropage.php?default=true&how=4\">click here </a><br/>\n";
+        break;
+
+        case "4":
+            if (db_fetch_assoc("select realm_id from user_auth_realm where user_id='" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id=8"))
+                echo "<b>Hint: </b>If you want to see Intropage plugin as default page (in console or separated tab), you can set it up in Console -> User Management -> User -> Login Options <br/>";
+            else
+                echo "<b>Hint: </b>If you want to see Graphs as default page <a href=\"" . $config['url_path'] . "plugins/intropage/intropage.php?default=true&how=3\">click here </a><br/>\n";
+        break;
+
+        case "5" :
+            if (db_fetch_assoc("select realm_id from user_auth_realm where user_id='" . $_SESSION["sess_user_id"] . "' and user_auth_realm.realm_id=8"))
+                echo "<b>Hint: </b>You can choose view between separated tab and console, you can set it up in Console -> User Management -> User -> Login Options <br/>";
+        break;
+    }
+}
+
 
 ?>
--- intropage.php
+++ intropage.php
@@ -4,19 +4,32 @@ chdir('../../');
 include_once("./include/auth.php");
 include_once("./plugins/intropage/display.php");
 
+
+// it works bad with user group, temporary disabled
+// set default page
+if (isset ($_GET["default"]) && $_GET["default"] == "true")             {
+
+    if (isset ($_GET["how"]))   {
+        $_GET["how"] = intval ($_GET["how"]);
+
+        if ($_GET["how"] >= 1 && $_GET["how"] <= 5 )
+        db_execute ("update user_auth set login_opts = ". $_GET["how"] . " where id = " . $_SESSION["sess_user_id"]);
+    }
+}
+
+
+
 set_default_action();
 
 
 $lopts = db_fetch_cell('SELECT intropage_opts FROM user_auth WHERE id=' . $_SESSION['sess_user_id']);
-if ($lopts == 1)
-//	top_header();	it shows console menu in separated tab - we don't need it
-	general_header();
+if ($lopts == 1)	// separated tab, we need header
+    general_header();
 
 display_informations();
-
 intropage_console_after();
 
-if ($lopts == 1)
+if ($lopts == 1)	// separated tab, we need footer
 	bottom_footer();
 
 ?>
