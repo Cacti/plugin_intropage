412b116011e7aa01cab137e133a42238faafd372
Petr Macek <petr.macek@kostax.cz>
2017-11-15 13:22:32 +0100

    remove debug echo

--- functions/analyse_db.php
+++ functions/analyse_db.php
@@ -17,7 +17,6 @@ function analyse_db() {
 	$tables = db_fetch_assoc ("SHOW TABLES");
 	foreach($tables as $key=>$val) {
 
-		echo "<br/>check table ".current($val)." $db_check_level";
 		$row = db_fetch_row("check table ".current($val)." $db_check_level");
 		if (preg_match('/^note$/i',$row["Msg_type"]) && preg_match('/doesn\'t support/i',$row["Msg_text"])) { $memtables++; }
 		elseif (!preg_match('/OK/i',$row["Msg_text"])) { $damaged++; $result['detail'] .= "Table " . $row["Table"] . " status " . $row["Msg_text"] . "<br/>\n"; }
