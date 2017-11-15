7b6a20f84e11ccccf2fb8fd3e12e74c5a1acbf59
Petr Macek <petr.macek@kostax.cz>
2017-11-15 13:21:50 +0100

    fix analyze db level parameter

--- functions/analyse_db.php
+++ functions/analyse_db.php
@@ -12,10 +12,12 @@ function analyse_db() {
 	
 	$damaged = 0;
 	$memtables = 0;
-	$db_check_level = read_config_option('intropage_db_check_level');
+	$db_check_level = read_config_option('intropage_analyse_db_level');
 	
 	$tables = db_fetch_assoc ("SHOW TABLES");
 	foreach($tables as $key=>$val) {
+
+		echo "<br/>check table ".current($val)." $db_check_level";
 		$row = db_fetch_row("check table ".current($val)." $db_check_level");
 		if (preg_match('/^note$/i',$row["Msg_type"]) && preg_match('/doesn\'t support/i',$row["Msg_text"])) { $memtables++; }
 		elseif (!preg_match('/OK/i',$row["Msg_text"])) { $damaged++; $result['detail'] .= "Table " . $row["Table"] . " status " . $row["Msg_text"] . "<br/>\n"; }
