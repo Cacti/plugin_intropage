ef4a350e567c7d3b2a8e2baeb666e0fd3812bbca
Petr Macek <petr.macek@kostax.cz>
2017-11-29 21:18:37 +0100

    format long OS info

--- functions/info.php
+++ functions/info.php
@@ -10,6 +10,8 @@ function info() {
 //		'detail' => '',
 	);
 	
+	$xdata = '';
+
 	if ($poller_options[read_config_option("poller_type")] == 'spine' && file_exists(read_config_option("path_spine")) && (function_exists('is_executable')) && (is_executable(read_config_option("path_spine")))) {
 	    $spine_version = "SPINE";
 	    exec(read_config_option("path_spine") . " --version", $out_array);
@@ -21,14 +23,20 @@ function info() {
 	} else {
 	    $result['data'] .= "Poller type: <a href=\"" . htmlspecialchars($config['url_path']) .  "settings.php?tab=poller\">".$poller_options[read_config_option("poller_type")]."</a><br/>";
 	}
-		
+
+	
+
 	$result['data'] .= "Running on: ";
 	if (function_exists("php_uname")) { 
-	    $result['data'] .= php_uname(); 
+	    $xdata = php_uname(); 
 	}
 	else { 
-	    $result['data'] .= PHP_OS; 
+	    $xdata .= PHP_OS; 
 	}
+	
+	$xdata2 = str_split ($xdata,50);
+	$xdata = join ("<br/>",$xdata2);	
+	$result['data'] .= $xdata;
 	    	
 	return $result;
 }
