f3bad91c9658fce026ac084ce67867f289a3ff98
Petr Macek <petr.macek@kostax.cz>
2017-12-26 20:48:10 +0100

    move function from analyse_log here

--- functions/common.php
+++ functions/common.php
@@ -1,5 +1,15 @@
 <?php
 
+function human_filesize($bytes, $decimals = 2) {
+        $size = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+}
+
+
+
+
+
 function intropage_display_panel ($type,$header,$dispdata)	{
 
    
