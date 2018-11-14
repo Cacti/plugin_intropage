686d415ee6d9f7ed072e296118ea1394e3947ecd
netniV <netniv@hotmail.com>
2018-11-14 09:53:20 +0000

    Apply coding style using php-cs-fixer

--- include/actions.php
+++ include/actions.php
@@ -1,126 +1,125 @@
 <?php
 
-if (isset_request_var('intropage_action') && 
-    get_filter_request_var('intropage_action', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^([a-z0-9_-]+)$/')))) {
+if (isset_request_var('intropage_action') &&
+	get_filter_request_var('intropage_action', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^([a-z0-9_-]+)$/')))) {
+	$values = explode('_', get_request_var('intropage_action'));
+	// few parameters from input type select has format reset_all, refresh_180, ... first is action
+	$action = array_shift($values);
+	$value  = implode('_', $values);
 
-    $values = explode ('_', get_request_var('intropage_action'));
-    // few parameters from input type select has format reset_all, refresh_180, ... first is action	
-    $action = array_shift($values);
-    $value = implode ("_", $values);
-    
-    switch ($action)	{
+	switch ($action) {
 
-	// close panel 
+	// close panel
 	case 'droppanel':
-	    if (get_filter_request_var('panel_id')) 
-		db_execute ('delete from plugin_intropage_user_setting where user_id = ' . $_SESSION['sess_user_id'] . ' and id = ' . get_request_var('panel_id'));
-	    // user can close debug panels like others
-	    if (get_request_var('panel_id') == 999)
-    		set_user_setting('intropage_debug', 0);
+		if (get_filter_request_var('panel_id')) {
+			db_execute('delete from plugin_intropage_user_setting where user_id = ' . $_SESSION['sess_user_id'] . ' and id = ' . get_request_var('panel_id'));
+		}
+		// user can close debug panels like others
+		if (get_request_var('panel_id') == 999) {
+			set_user_setting('intropage_debug', 0);
+		}
 	break;
 
 
 	// favourite graphs
 	case 'favgraph':
-	    if (get_filter_request_var('graph_id'))	{
-        
+		if (get_filter_request_var('graph_id')) {
+
 		// already fav?
-		if (db_fetch_cell ('select count(*) from plugin_intropage_user_setting where user_id=' . $_SESSION['sess_user_id'] . 
-				    ' and fav_graph_id=' . get_request_var('graph_id')) > 0)	{	
-	
-		    db_execute ('delete from plugin_intropage_user_setting where user_id=' . $_SESSION['sess_user_id'] . ' and fav_graph_id=' .  get_request_var('graph_id')); 
+			if (db_fetch_cell('select count(*) from plugin_intropage_user_setting where user_id=' . $_SESSION['sess_user_id'] .
+					' and fav_graph_id=' . get_request_var('graph_id')) > 0) {
+				db_execute('delete from plugin_intropage_user_setting where user_id=' . $_SESSION['sess_user_id'] . ' and fav_graph_id=' .  get_request_var('graph_id'));
+			} else {	// add to fav
+				// priority for new panel:
+				$prio = db_fetch_cell("select priority from plugin_intropage_panel where panel='intropage_favourite_graph'");
+
+				db_execute('insert into plugin_intropage_user_setting (user_id,priority,panel,fav_graph_id) values (' . $_SESSION['sess_user_id'] .
+				",$prio,'intropage_favourite_graph'," . get_request_var('graph_id') . ')');
+			}
 		}
-		else	{	// add to fav
-		    // priority for new panel:
-		    $prio = db_fetch_cell ("select priority from plugin_intropage_panel where panel='intropage_favourite_graph'");
-		    
-		    db_execute ("insert into plugin_intropage_user_setting (user_id,priority,panel,fav_graph_id) values (" . $_SESSION['sess_user_id'] . 
-				",$prio,'intropage_favourite_graph'," . get_request_var('graph_id') . ")"); 
-		}
-	    }
 	break;
 
 	// panel order
 	case 'order':
-	    if (isset_request_var('xdata'))  {
-		$error = false;
-		$order = array();
-		foreach (get_request_var('xdata') as $data)   {
-    		    list($a,$b) = explode ('_',$data);
-    		    if (filter_var($b, FILTER_VALIDATE_INT))    {
-        		array_push ($order, $b);
-    		    }
-    		    else    {
-        		$error = true;
-    		    }
+		if (isset_request_var('xdata')) {
+			$error = false;
+			$order = array();
+			foreach (get_request_var('xdata') as $data) {
+				list($a, $b) = explode('_', $data);
+				if (filter_var($b, FILTER_VALIDATE_INT)) {
+					array_push($order, $b);
+				} else {
+					$error = true;
+				}
 
-    		    if (!$error)    {
-        		$_SESSION['intropage_order'] = $order;
-        		$_SESSION['intropage_changed_order'] = true;
-    		    }
+				if (!$error) {
+					$_SESSION['intropage_order']         = $order;
+					$_SESSION['intropage_changed_order'] = true;
+				}
+			}
 		}
-	    }
 
 
 	break;
 
 	// reset all panels
 	case 'reset':
-	    if ($value == 'all')	{
-    		unset ($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
-		db_execute ('delete from plugin_intropage_user_setting where user_id = ' . $_SESSION['sess_user_id']);
-		// default values
-		set_user_setting('intropage_display_important_first', read_config_option('intropage_display_important_first'));
-		set_user_setting('intropage_display_level', read_config_option('intropage_display_level'));
-		set_user_setting('intropage_autorefresh', read_config_option('intropage_autorefresh'));
-	    }
-	    elseif ($value == 'order')
-		unset ($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
+		if ($value == 'all') {
+			unset($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
+			db_execute('delete from plugin_intropage_user_setting where user_id = ' . $_SESSION['sess_user_id']);
+			// default values
+			set_user_setting('intropage_display_important_first', read_config_option('intropage_display_important_first'));
+			set_user_setting('intropage_display_level', read_config_option('intropage_display_level'));
+			set_user_setting('intropage_autorefresh', read_config_option('intropage_autorefresh'));
+		} elseif ($value == 'order') {
+			unset($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
+		}
 	break;
-    
+
 	case 'addpanel':
-	    if (preg_match('/^[a-z0-9\-\_]+$/i', $value))	{
-		db_execute ('insert into plugin_intropage_user_setting (user_id,panel,priority) select ' . $_SESSION['sess_user_id'] . ',panel,priority from plugin_intropage_panel where panel=\'' . $value . '\' limit 1');
-	    }
+		if (preg_match('/^[a-z0-9\-\_]+$/i', $value)) {
+			db_execute('insert into plugin_intropage_user_setting (user_id,panel,priority) select ' . $_SESSION['sess_user_id'] . ',panel,priority from plugin_intropage_panel where panel=\'' . $value . '\' limit 1');
+		}
 	break;
 
 	case 'refresh':
-	    if ($value == 0 || $value == 60 || $value == 180 || $value == 600)
-		    set_user_setting('intropage_autorefresh', $value);
+		if ($value == 0 || $value == 60 || $value == 180 || $value == 600) {
+			set_user_setting('intropage_autorefresh', $value);
+		}
 	break;
 
 	case 'debug':
-	    if ($value == 'ena')
-		    set_user_setting('intropage_debug', 1);
-	    if ($value == 'disa')
-		    set_user_setting('intropage_debug', 0);
+		if ($value == 'ena') {
+			set_user_setting('intropage_debug', 1);
+		}
+		if ($value == 'disa') {
+			set_user_setting('intropage_debug', 0);
+		}
 	break;
 
 	case 'important':
-		if ($value == 'first')		{
-		    set_user_setting('intropage_display_important_first', 'on');
-		    unset ($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
-		}
-		else	{
-		    set_user_setting('intropage_display_important_first', 'off');
-		    unset ($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
+		if ($value == 'first') {
+			set_user_setting('intropage_display_important_first', 'on');
+			unset($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
+		} else {
+			set_user_setting('intropage_display_important_first', 'off');
+			unset($_SESSION['intropage_changed_order'], $_SESSION['intropage_order']);
 		}
 	break;
 
 	case 'displaylevel':
-	    if (preg_match('/^[0-9]{1}$/', $value))
-		set_user_setting('intropage_display_level', $value);
+		if (preg_match('/^[0-9]{1}$/', $value)) {
+			set_user_setting('intropage_display_level', $value);
+		}
 	break;
-    
+
 
 	case 'loginopt':
-	    if ($value == 'intropage') // SELECT login_opts FROM user_auth WHERE id
-		db_fetch_cell_prepared('update user_auth set login_opts=4 WHERE id=?',array($_SESSION['sess_user_id']));
-	    elseif ($value == "graph")
-		db_fetch_cell_prepared('update user_auth set login_opts=3 WHERE id=?',array($_SESSION['sess_user_id']));
-    
-    }
+		if ($value == 'intropage') { // SELECT login_opts FROM user_auth WHERE id
+			db_fetch_cell_prepared('update user_auth set login_opts=4 WHERE id=?', array($_SESSION['sess_user_id']));
+		} elseif ($value == 'graph') {
+			db_fetch_cell_prepared('update user_auth set login_opts=3 WHERE id=?', array($_SESSION['sess_user_id']));
+		}
 
+	}
 }
-
-?>
--- include/variables.php
+++ include/variables.php
@@ -2,31 +2,31 @@
 
 // priority (order) (bigger number =  highest priority)
 
-// maint plugin and admin alert panels are always visible 
+// maint plugin and admin alert panels are always visible
 
-$panel = array();
-$panel['intropage_analyse_log']['priority'] = 60;
-$panel['intropage_analyse_login']['priority'] = 75;
-$panel['intropage_thold_event']['priority'] = 77;
-$panel['intropage_analyse_db']['priority'] = 70;
+$panel                                                  = array();
+$panel['intropage_analyse_log']['priority']             = 60;
+$panel['intropage_analyse_login']['priority']           = 75;
+$panel['intropage_thold_event']['priority']             = 77;
+$panel['intropage_analyse_db']['priority']              = 70;
 $panel['intropage_analyse_tree_host_graph']['priority'] = 50;
-$panel['intropage_trend']['priority'] = 30;
-$panel['intropage_extrem']['priority'] = 30;
-$panel['intropage_ntp']['priority'] = 60;
-$panel['intropage_poller_info']['priority'] = 50;
-$panel['intropage_poller_stat']['priority'] = 50;
-$panel['intropage_graph_host']['priority'] = 14;
-$panel['intropage_graph_thold']['priority'] = 13;
-$panel['intropage_graph_data_source']['priority'] = 12;
-$panel['intropage_graph_host_template']['priority'] = 11;
-$panel['intropage_cpu']['priority'] = 45;
-$panel['intropage_mactrack']['priority'] = 42;
-$panel['intropage_mactrack_sites']['priority'] = 43;
-$panel['intropage_top5_ping']['priority'] = 30;
-$panel['intropage_top5_availability']['priority'] = 31;
-$panel['intropage_info']['priority'] = 1;
-$panel['intropage_boost']['priority'] = 55;
-$panel['intropage_favourite_graph']['priority'] = 95;
+$panel['intropage_trend']['priority']                   = 30;
+$panel['intropage_extrem']['priority']                  = 30;
+$panel['intropage_ntp']['priority']                     = 60;
+$panel['intropage_poller_info']['priority']             = 50;
+$panel['intropage_poller_stat']['priority']             = 50;
+$panel['intropage_graph_host']['priority']              = 14;
+$panel['intropage_graph_thold']['priority']             = 13;
+$panel['intropage_graph_data_source']['priority']       = 12;
+$panel['intropage_graph_host_template']['priority']     = 11;
+$panel['intropage_cpu']['priority']                     = 45;
+$panel['intropage_mactrack']['priority']                = 42;
+$panel['intropage_mactrack_sites']['priority']          = 43;
+$panel['intropage_top5_ping']['priority']               = 30;
+$panel['intropage_top5_availability']['priority']       = 31;
+$panel['intropage_info']['priority']                    = 1;
+$panel['intropage_boost']['priority']                   = 55;
+$panel['intropage_favourite_graph']['priority']         = 95;
 
 $intropage_settings = array(	// default values
 
@@ -44,14 +44,14 @@ $intropage_settings = array(	// default values
 		'friendly_name' => 'Automatic refresh page',
 		'description' => 'How often',
 		'method' => 'drop_array',
-		'array' => array('0' => 'Never', '60' => 'Every minute', '180' => 'Every 3 minutes', '600' => 'Every 10 minutes',),
+		'array' => array('0' => 'Never', '60' => 'Every minute', '180' => 'Every 3 minutes', '600' => 'Every 10 minutes', ),
 		'default' => '0',
 	),
 	'intropage_display_level' => array(
 		'friendly_name' => 'Display',
 		'description' => 'What will you see',
 		'method' => 'drop_array',
-		'array' => array('0' => 'Only errors', '1' => 'Errors and warnings', '2' => 'All',),
+		'array' => array('0' => 'Only errors', '1' => 'Errors and warnings', '2' => 'All', ),
 		'default' => '2',
 	),
 	'intropage_analyse_log_rows' => array(
@@ -61,12 +61,12 @@ $intropage_settings = array(	// default values
 		'max_length' => 5,
 		'default' => '1000',
 	),
-	
+
 	'intropage_analyse_db_level' => array(
 		'friendly_name' => 'Analyse DB - Level of db check',
 		'description' => 'Quick - no check rows for inforccert links<br/>Fast - check only not properly closed tables<br/>Changed - check tables changed from last check<br/>Medium - with rows scan<br/>Extended - full rows and keys<br/><strong>Medium and extended may causes slow page load!</strong>',
 		'method' => 'drop_array',
-		'array' => array('QUICK' => 'Quick', 'FAST' => 'Fast', 'CHANGED' => 'Changed', 'MEDIUM' => 'Medium', 'EXTENDED'  => 'Extended'),
+		'array' => array('QUICK' => 'Quick', 'FAST' => 'Fast', 'CHANGED' => 'Changed', 'MEDIUM' => 'Medium', 'EXTENDED' => 'Extended'),
 		'default' => 'Changed',
 	),
 
@@ -83,8 +83,8 @@ $intropage_settings = array(	// default values
 		'description' => 'If isn\'t empty, panel will be displayed on the top. You can use html tags (b, i, ...).',
 		'method' => 'textarea',
 		'max_length' => 1000,
-                'textarea_rows' => '4',
-                'textarea_cols' => '60',
+				'textarea_rows' => '4',
+				'textarea_cols' => '60',
 		'default' => '',
 	),
 
@@ -98,5 +98,3 @@ $intropage_settings = array(	// default values
 
 
 );
-
-?>
