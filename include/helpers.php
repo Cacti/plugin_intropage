3cedc29ece7b54d7ef45355e7c2b108be1ae8889
Petr Macek <petr.macek@kostax.cz>
2018-12-11 21:58:25 +0100

    prepare for copy text from panels

--- include/helpers.php
+++ include/helpers.php
@@ -89,6 +89,7 @@ function intropage_display_panel($panel_id, $type, $header, $dispdata) {
 
 	// awesome 5.0+    printf("<a href='#' title='Reload panel - not implemented' class='header_link'><i class='fa fa-sync-alt'></i></a>\n");
 
+
 	if ($panel_id > 990) {
 		printf("<a href='#' title='You cannot disable this panel' class='header_link'>&nbsp; <i class='fa fa-times'></i></a>\n");
 	} else {
@@ -99,6 +100,9 @@ function intropage_display_panel($panel_id, $type, $header, $dispdata) {
 	if (isset($dispdata['detail']) && !empty($dispdata['detail'])) {
 		printf("<a href='#' title='Show details' class='header_link maxim' name='%s'><i class='fa fa-window-maximize'></i></a>\n", md5($header));
 	}
+
+	printf("<a href='#' title='Move panel/disable copy text - comming soon' class='header_link'><i class='fa fa-clone'></i></a>\n");
+
 
 	print " </div>\n";
 	print "	<table class='cactiTable'>\n";
