941670794908759c53c4b511495c760e3c07a47e
Petr Macek <petr.macek@kostax.cz>
2019-01-06 21:45:25 +0100

    prepare for single panel reload

--- intropage_ajax.php
+++ intropage_ajax.php
@@ -2,7 +2,16 @@
 
 chdir('../../');
 include_once('./include/auth.php');
-include_once('./plugins/intropage/display.php');
 
 
-display_information();
+
+if (isset_request_var('reload_panel') &&
+        get_filter_request_var('reload_panel', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[0-9]{1,3}$/')))) {
+// reload jednoho panelu
+    echo "<b>Here will be new panel content - comming soon</b><br/>actual time" . date ("H:m:i");
+    echo "panel id " . get_request_var('reload_panel');
+}
+else	{	// reload all
+    include_once('./plugins/intropage/display.php');
+    display_information();
+}
