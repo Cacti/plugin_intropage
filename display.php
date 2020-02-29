0f1b89304a4204a5e467fc8d5cb74b3b4afc8874
Petr Macek <petr.macek@kostax.cz>
2020-02-29 22:23:15 +0100

    Header date update

--- display.php
+++ display.php
@@ -1,7 +1,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2015-2019 Petr Macek                                      |
+ | Copyright (C) 2015-2020 Petr Macek                                      |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
