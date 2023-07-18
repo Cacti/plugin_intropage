a9d6ad1c7da0f6beddefa5c40f14431eaa665caa
Petr Macek <petr.macek@kostax.cz>
2023-07-18 14:09:16 +0200

    fix boost - php error (#272)
    * fix boost - php error
    
    * remove old code

--- panellib/system.php
+++ panellib/system.php
@@ -302,7 +302,11 @@ function boost($panel, $user_id) {
 	if ($boost_status != '') {
 		$boost_status_array = explode(':', $boost_status);
 
-		$boost_status_date  = $boost_status_array[1];
+		if (isset($boost_status_array[1])) {
+			$boost_status_date = $boost_status_array[1];
+		} else {
+			$boost_status_date = null;
+		}
 
 		if (substr_count($boost_status_array[0], 'complete')) {
 			$boost_status_text = __('Idle', 'intropage');
