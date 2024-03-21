a9ea1153e3c0d3fadd7ee6a5f3648dfe8e1b19aa
xmacan <petr.macek@kostax.cz>
2024-03-21 16:23:01 +0100

    fix maint-summertime, fix #294

--- panellib/misc.php
+++ panellib/misc.php
@@ -225,9 +225,21 @@ function maint($panel, $user_id) {
 
 						break;
 					case 2:
-						while ($sc['etime'] < $t) {
-							$sc['etime'] += $sc['minterval'];
-							$sc['stime'] += $sc['minterval'];
+						/* past, calculate next */
+						if ($sc['etime'] < $t) {
+							/* convert start and end to local so that hour stays same for add days across daylight saving time change */
+							$starttimelocal = (new DateTime('@' . strval($sc['stime'])))->setTimezone( new DateTimeZone( date_default_timezone_get()));
+							$endtimelocal   = (new DateTime('@' . strval($sc['etime'])))->setTimezone( new DateTimeZone( date_default_timezone_get()));
+							$nowtime        = new DateTime();
+							/* add interval days */
+							$addday = new DateInterval( 'P' . strval($sc['minterval'] / 86400) . 'D');
+							while ($endtimelocal < $nowtime) {
+								$starttimelocal = $starttimelocal->add( $addday );
+								$endtimelocal   = $endtimelocal->add( $addday );
+							}
+
+							$sc['stime'] = $starttimelocal->getTimestamp();
+							$sc['etime'] = $endtimelocal->getTimestamp();
 						}
 
 						if ($t > ($sc['stime'] - $maint_days_before) && $t < $sc['etime']) {
@@ -288,7 +300,8 @@ function webseer($panel, $user_id) {
 			$panel['alarm'] = 'red';
 		}
 
-		$panel['data']  = __('Number of checks (all/disabled): ', 'intropage') . $all . ' / ' . $disa . '<br/>';
+		$panel['data']  = '<b>' . __('Webseer plugin no longer supported, use plugin servcheck instead', 'intropage') . '</b><br/>';
+		$panel['data'] .= __('Number of checks (all/disabled): ', 'intropage') . $all . ' / ' . $disa . '<br/>';
 		$panel['data'] .= __('Status (up/down): ', 'intropage') . $ok . ' / ' . $ko . '<br/><br/>';
 
 		$logs = db_fetch_assoc ('SELECT pwul.lastcheck, pwul.result, pwul.http_code, pwul.error, pwu.url,
