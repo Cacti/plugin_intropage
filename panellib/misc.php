bb95b42c7ed696b9bd2144bb297953e9ec7d4049
Petr Macek <petr.macek@kostax.cz>
2025-09-26 23:29:16 +0200

    fix new name in servcheck (#345)

--- panellib/misc.php
+++ panellib/misc.php
@@ -486,7 +486,15 @@ function servcheck($panel, $user_id) {
 		$all  = db_fetch_cell('SELECT COUNT(*) FROM plugin_servcheck_test');
 		$disa = db_fetch_cell("SELECT COUNT(*) FROM plugin_servcheck_test WHERE enabled != 'on'");
 
-		$tests = db_fetch_assoc('SELECT display_name, type, id, lastcheck FROM plugin_servcheck_test');
+		$column = db_fetch_cell("SELECT COLUMN_NAME FROM information_schema.columns
+			WHERE TABLE_NAME = 'plugin_servcheck_test' AND COLUMN_NAME = 'display_name'");
+
+		// servcheck < 0.3 uses another name
+		if ($column) {
+			$tests = db_fetch_assoc('SELECT display_name as name, type, id, lastcheck FROM plugin_servcheck_test');
+		} else {
+			$tests = db_fetch_assoc('SELECT name, type, id, lastcheck FROM plugin_servcheck_test');
+		}
 			
 		foreach ($tests as $test) {
 			$state = db_fetch_cell_prepared('SELECT result FROM plugin_servcheck_log
@@ -506,12 +514,22 @@ function servcheck($panel, $user_id) {
 
 		$panel['data'] .= __('Number of checks (all/disabled): ', 'intropage') . $all . ' / ' . $disa . '<br/>';
 		$panel['data'] .= __('Status (ok/error): ', 'intropage') . $ok . ' / ' . $ko . '<br/><br/>';
-		$logs = db_fetch_assoc ('SELECT psl.lastcheck as `lastcheck`, result, error, display_name, type,
-			UNIX_TIMESTAMP(psl.lastcheck) AS secs
-			FROM plugin_servcheck_log AS psl
-			LEFT JOIN plugin_servcheck_test AS pst
-			ON psl.test_id = pst.id 
-			LIMIT ' . ($lines - 4));
+
+		if ($column) {
+			$logs = db_fetch_assoc ('SELECT psl.lastcheck as `lastcheck`, result, error, display_name as name, type,
+				UNIX_TIMESTAMP(psl.lastcheck) AS secs
+				FROM plugin_servcheck_log AS psl
+				LEFT JOIN plugin_servcheck_test AS pst
+				ON psl.test_id = pst.id 
+				LIMIT ' . ($lines - 4));
+		} else {
+			$logs = db_fetch_assoc ('SELECT psl.lastcheck as `lastcheck`, result, error, name, type,
+				UNIX_TIMESTAMP(psl.lastcheck) AS secs
+				FROM plugin_servcheck_log AS psl
+				LEFT JOIN plugin_servcheck_test AS pst
+				ON psl.test_id = pst.id 
+				LIMIT ' . ($lines - 4));
+		}
 
 		if (cacti_sizeof($logs) > 0) {
 
@@ -551,7 +569,7 @@ function servcheck($panel, $user_id) {
 				}
 
 				$panel['data'] .= '<td class="rpad">' . $row['lastcheck'] . '</td>' .
-					'<td class="rpad">' . $row['display_name'] . '</td>' .
+					'<td class="rpad">' . $row['name'] . '</td>' .
 					'<td class="rpad">' . $row['type'] . '</td>' .
 					'<td class="rpad"><span class="inpa_sq color_' . $color . '"></span>' . $row['result'] .'</td></tr>';
 			}
