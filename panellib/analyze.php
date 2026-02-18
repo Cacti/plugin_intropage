bf895cd2aa672bde4a99a864a378c7b010592dd7
xmacan <petr.macek@kostax.cz>
2026-02-18 20:42:19 +0100

    fix #359 - incorrect value

--- panellib/analyze.php
+++ panellib/analyze.php
@@ -132,7 +132,7 @@ function register_analyze() {
 function analyse_login($panel, $user_id) {
 	global $config;
 
-	$lines            = read_user_setting('intropage_number_of_lines', read_config_option('intropage_number_of_lines'), false, $user_id);
+	$lines            = intropage_get_lines($user_id);
 	$important_period = read_user_setting('intropage_important_period', read_config_option('intropage_important_period'), false, $user_id);
 
 	if ($important_period == -1) {
